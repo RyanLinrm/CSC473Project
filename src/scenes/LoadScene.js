a62fcbcfc3d17b6b22762e5fa0c48d386966e85c
Hongjie Huang <hongjie824@gmail.com>
2019-03-18 17:14:02 -0400

    add needed function

---
+++ src/scenes/LoadScene.js
@@ -0,0 +1,18 @@
+import {CST} from "../CST";
+export class LoadScene extends Phaser.Scene{
+    constructor(){
+        super({
+            key: CST.SCENES.LOAD
+        })
+    }
+    init(){
+        
+    }
+
+    preload(){
+   
+    }
+
+    create(){
+    }
+}
