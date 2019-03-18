74a6bc73536eb3f8a0a450446487357b5f24f53f
Hongjie Huang <hongjie824@gmail.com>
2019-03-18 17:25:40 -0400

    modify preload function

--- src/scenes/LoadScene.js
+++ src/scenes/LoadScene.js
@@ -10,6 +10,19 @@ export class LoadScene extends Phaser.Scene{
     }
 
     preload(){
+
+        //add Loading image, sound and spritesheet
+        this.load.image('key1', './assets/title_bg.jpg');
+        this.load.image("StartButton", "./assets/StartButton.png");
+        this.load.image("cursor", "./assets/fight.png");
+        this.load.image("fire","./assets/SkillEffect1.png");
+
+        this.load.atlas("magic", "./assets/sprite/magic.png", "./assets/sprite/magic.json");
+        this.load.atlas("wolf", "./assets/sprite/wolf.png", "./assets/sprite/wolf.json");
+
+        this.load.audio("menuMusic", "./assets/music/Rise of spirit.mp3");
+
+        this.load.audio("beginsound", "./assets/soundeffect/metal-clash.wav");
    
     }
 
