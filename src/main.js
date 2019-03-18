5a458368fcc0f89ca987cb8be8124b62a41f81c4
RyanLinrm <runminlin@gmail.com>
2019-03-18 18:13:37 -0400

    Added init play scene

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
+/** @type {import { "../typing/phaser" };} */
+
 import { LoadScene } from "./scenes/LoadScene";
 import { MenuScene } from "./scenes/MenuScene";
-
-/** @type {import { "../typing/phaser" };} */
+import { PlayScene } from "./scenes/PlayScene";
 
 let game = new Phaser.Game({
     type: Phaser.AUTO,
     width:800,
     height:600,
     scene:[
-        LoadScene,MenuScene
+        LoadScene,MenuScene,PlayScene
     ]
 });
---
+++ src/scenes/PlayScene.js
@@ -0,0 +1,23 @@
+//Play scene for our basic playground
+//All the functions and character added in this file will be temporary
+//just for demo
+//Use scene list to generate keyword
+import { CST } from "../CST";
+export class PlayScene extends Phaser.Scene{
+    
+    constructor(){
+        super({key:CST.SCENES.PLAY});
+    }
+
+    init(){
+
+    }
+
+    preload(){ 
+    }
+
+    create(){
+
+    }
+
+}
