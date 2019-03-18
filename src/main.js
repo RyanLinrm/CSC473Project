78e5d558fba3694b598004b61c9c62a1ddf3a6aa
Hongjie Huang <hongjie824@gmail.com>
2019-03-18 17:49:42 -0400

    add scenes to main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
+import { LoadScene } from "./scenes/LoadScene";
+import { MenuScene } from "./scenes/MenuScene";
+
 /** @type {import { "../typing/phaser" };} */
 
 let game = new Phaser.Game({
     type: Phaser.AUTO,
     width:800,
-    height:600
+    height:600,
+    scene:[
+        LoadScene,MenuScene
+    ]
 });
