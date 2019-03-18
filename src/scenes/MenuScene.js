d7f251542cd3a8e0ba26c728b6bf0b517b1952c3
Hongjie Huang <hongjie824@gmail.com>
2019-03-18 17:41:38 -0400

    init menuscene

---
+++ src/scenes/MenuScene.js
@@ -0,0 +1,18 @@
+import { CST } from "../CST";
+export class MenuScene extends Phaser.Scene{
+    constructor(){
+        super({
+            key: CST.SCENES.MENU
+        })
+    }
+    init(){
+
+    }
+    preload(){
+
+    }
+    
+    create(){
+    }
+
+}
