c9da887958eed306b542a275f2a558cf03fc6dc2
RyanLinrm <runminlin@gmail.com>
2019-03-24 18:42:44 -0400

    Added in our frist demo map, tile map

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -9,13 +9,19 @@ export class PlayScene extends Phaser.Scene{
     }
 
     preload(){ 
+        this.load.image("tiles1", "./assets/tiles/map_atlas.png");
+        this.load.image("tiles2", "./assets/tiles/map_atlas2.png");
+
+        this.load.tilemapTiledJSON("Mymap","./assets/map/map.json");
     }
 
     create(){
         //create phaser game object, and add in sprite
-        this.player = this.physics.add.sprite(400, 400, "magic", "Magic_01.png" );
+        this.player = this.physics.add.sprite(300, 300, "magic", "Magic_01.png" );
 
-        this.wolf = this.physics.add.sprite(300, 300, "wolf", "Wolf_01.png" );
+        this.wolf = this.physics.add.sprite(100, 100, "wolf", "Wolf_01.png" );
+
+        this.player.setCollideWorldBounds(true);
 
         //create animations for different directions 
         
@@ -65,6 +71,20 @@ export class PlayScene extends Phaser.Scene{
 
         //input and phyics
         this.keyboard = this.input.keyboard.addKeys("W, A, S, D");
+
+        
+        //add in our map
+        let Mymap = this.add.tilemap("Mymap");
+
+        let tiles1 = Mymap.addTilesetImage("map_atlas", "tiles1");
+        let tiles2 = Mymap.addTilesetImage("map_atlas2", "tiles2");
+
+        //display layers
+        let groundLayer = Mymap.createStaticLayer("GroundLayer", [tiles1], 0 , 0).setDepth(-1);
+        let centerLayer = Mymap.createStaticLayer("Center", [tiles2], 0 , 0).setDepth(-1);
+        let waterLayer = Mymap.createStaticLayer("Water", [tiles1], 0 , 0).setDepth(-1);
+        let objectLayer = Mymap.createStaticLayer("Objects", [tiles1], 0 , 0).setDepth(-1);
+        let addonLayer = Mymap.createStaticLayer("AddOn", [tiles1], 0 , 0).setDepth(-1);
 
 
     }
