86fce8eae29486250d21c511dee25ebf44ff5613
nmaswood98 <30912532+nmaswood98@users.noreply.github.com>
2019-03-26 23:37:48 -0400

    Created  Player class. Moved attack functionality inside player class

---
+++ src/gameObjects/Player.js
@@ -0,0 +1,34 @@
+import { Bullet } from "../gameObjects/Projectiles";
+
+export class Player extends Phaser.Physics.Arcade.Sprite{
+    constructor(scene,x,y,key,textureName){
+        super(scene,x,y,key,textureName);
+
+        //adds to the scenes update and display list
+        scene.sys.updateList.add(this);
+        scene.sys.displayList.add(this);
+
+        this.setOrigin(0,0);
+        this.nonZeroVelocity = {x:0,y:1};
+
+        //enables body in the phsyics world in the game
+        scene.physics.world.enableBody(this);
+        this.createWeapon(scene);
+
+        scene.input.on('pointerdown',()=>{ //pointerdown event handler
+            this.attack();
+        });
+    }
+
+    createWeapon(scene){
+        let bullets = scene.physics.add.group({classType: Bullet, runChildUpdate: true});
+
+        this.attack = ()=>{
+            let bullet = bullets.get();
+            scene.children.add(bullet);
+            bullet.shoot(this,scene.input.x,scene.input.y);
+          };
+
+    }
+
+}
