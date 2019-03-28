e5fb728da5d830efa032cd31ed169513f78b3110
nmaswood98 <30912532+nmaswood98@users.noreply.github.com>
2019-03-27 21:38:55 -0400

    Added the initial socketio code that establishes connection between client and tells the client about all the other plaeyrs that are on the server

--- GameServer/server/authoritative_server/js/game.js
+++ GameServer/server/authoritative_server/js/game.js
@@ -2,6 +2,8 @@
 
 /** @type {import { "../typing/phaser" };} */
 
+
+
 let game = new Phaser.Game({
     type: Phaser.HEADLESS,
     parent: 'phaser-example',
@@ -28,13 +30,29 @@ function preload(){
    
 }
 
+
 function create(){
     //socket.io connection 
+    this.players = {};
+
+
    io.on('connection',(socket)=>{
-       console.log("User connect");
+      
+
+       //add player 
+        this.players[socket.id] = {
+            id: socket.id,
+            x:0,
+            y:0,
+            rotation:0
+        }
+        socket.emit("connection",this.players);
+        socket.broadcast.emit("newPlayer",this.players[socket.id]);
 
        socket.on('disconnect',()=>{
             console.log("user disconnect");
+            delete this.players[socket.id];
+            io.emit("playerDisconnect",socket.id);
        });
    });
 
--- GameServer/server/public/js/game.js
+++ GameServer/server/public/js/game.js
@@ -26,6 +26,21 @@ function preload(){
 
 function create(){
     this.socket = io();
+
+    this.socket.on("connection",(players)=>{
+        //InitialPlayerConnection
+        //Going to map each player in players into a sprite and place them in proper position on the screen
+    });
+
+    this.socket.on("newPlayer",(player)=>{
+        //New player Joined the game
+        //Add player to the postiion 
+    });
+
+    this.socket.on("playerDisconnect",()=>{
+        //A player has disconnnected handle logic towards that
+        //In this game it may be good to replace the player with a bot. 
+    });
 };
 
 function update(){
