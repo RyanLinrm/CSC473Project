5da99db4e81bd566c2657d885afe2da20df2f4f6
Hongjie Huang <hongjie824@gmail.com>
2019-04-03 23:23:28 -0400

    add speed up and change size skill key

--- ReactProject/src/PhaserGame/scenes/PlayScene.js
+++ ReactProject/src/PhaserGame/scenes/PlayScene.js
@@ -26,19 +26,21 @@ export class PlayScene extends Phaser.Scene{
         //Create an enemygroup with runChildUpdate set to true. Every enemy added to this group will have its update function then called. 
         //Without this groupt the update funciton would not be called for the enemies
         this.enemyGroup = this.add.group({runChildUpdate: true}); 
-
+        this.bullets = this.add.group({runChildUpdate: true}); 
         //create phaser game object, and add in sprite
+  
+        this.player = new Player(this,300,300, "p1", "p1_01.png",10);
 
-        this.player = new Player(this,300,300, "p1", "p1_01.png");
-
-        //The enemies wolf and angel. 
-        this.wolf = new Enemy(this,100,100, "wolf", "Wolf_01.png",this.player);
-        this.ninjabot= new Enemy(this,200,150,"ninjabot","ninjabot_1.png",this.player);
-        this.demon1=new Enemy(this,400,300,"demon1","demon1_01").setScale(1.5);
+        //adjust player hit box
+        this.player.setSize( 24, 28).setOffset(5,5);
+        //The enemies  
+        this.wolf = new Enemy(this,100,100, "wolf", "Wolf_01.png",this.player,10);
+        this.ninjabot= new Enemy(this,200,150,"ninjabot","ninjabot_1.png",this.player,10);
+        this.demon1=new Enemy(this,575,500,"demon1","demon1_01").setScale(1.5);
         this.enemyGroup.add(this.wolf);
         this.enemyGroup.add(this.ninjabot);
 
-
+       
         //create a sample minimap ---needs to change to dynamic
         this.minimap = this.cameras.add(590, 5, 250, 150).setZoom(0.2).setName('mini');
         this.minimap.setBackgroundColor(0x002244);
@@ -59,7 +61,9 @@ export class PlayScene extends Phaser.Scene{
         this.sword_in_the_stone=new Units(this,645,645,"sword_in_the_stone");
         this.sword_in_the_stone.setScale(0.5);
         this.player.setCollideWorldBounds(true);
-    
+        this.physics.add.collider(this.enemyGroup, this.enemyGroup);
+        this.physics.add.collider(this.player, this.enemyGroup);
+       
        //set up attacking animation check
        this.anims.create({
         key: "ab2",
@@ -71,10 +75,7 @@ export class PlayScene extends Phaser.Scene{
         }),
         repeat: -1
      });
-        this.add.sprite(425, 300, 'a2_01').play('ab2');
-
-
- 
+        this.add.sprite(600, 500, 'a2_01').play('ab2');
 
 
 
@@ -128,10 +129,13 @@ export class PlayScene extends Phaser.Scene{
         });
 
         //input and phyics
-        this.keyboard = this.input.keyboard.addKeys("W, A, S, D");
+        this.keyboard = this.input.keyboard.addKeys("W, A, S, D,SHIFT");
         this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        this.Rbar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        this.Tbar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.T);
 
-        
+        //Map
+
         //add in our map
         let Mymap = this.add.tilemap("Mymap");
 
@@ -139,14 +143,36 @@ export class PlayScene extends Phaser.Scene{
         let tiles2 = Mymap.addTilesetImage("map_atlas2", "tiles2");
 
        // display layers
-        let groundLayer = Mymap.createStaticLayer("GroundLayer", [tiles1], 0 , 0).setDepth(-1);
+        let groundLayer = Mymap.createStaticLayer("GroundLayer", [tiles1], 0 , 0).setDepth(-3);
         let centerLayer = Mymap.createStaticLayer("Center", [tiles2], 0 , 0).setDepth(-1);
-        let waterLayer = Mymap.createStaticLayer("Water", [tiles1], 0 , 0).setDepth(-1);
+        let waterLayer = Mymap.createStaticLayer("Water", [tiles1], 0 , 0).setDepth(-2);
         let objectLayer = Mymap.createStaticLayer("Objects", [tiles1], 0 , 0).setDepth(-1);
         let addonLayer = Mymap.createStaticLayer("AddOn", [tiles1], 0 , 0).setDepth(-1);
+        let CollisionLayer = Mymap.createStaticLayer("Collision",[tiles1], 0, 0);
+
+        //Collision layer handler
+        CollisionLayer.setCollisionByProperty({collides:true});
+        waterLayer.setCollisionByProperty({collides:true});
+
+        //Assign collider objects
+        this.physics.add.collider(this.player, CollisionLayer);
+        this.physics.add.collider(this.player, waterLayer);
+        this.physics.add.collider(this.enemyGroup, waterLayer);
+        this.physics.add.collider(this.enemyGroup, CollisionLayer);
+  
+        //Map collision debug mode
+        this.debugGraphics = this.add.graphics();
+ 
+        Mymap.renderDebug(this.debugGraphics, {
+            tileColor: null,
+            collidingTileColor: new Phaser.Display.Color(243, 134, 48, 128), 
+            faceColor: new Phaser.Display.Color(40, 39, 37, 255) 
+          });
+
 
 
         //Camera
+
         // set bounds to avoid camera goes outside the map
         this.physics.world.setBounds(0, 0, Mymap.widthInPixels, Mymap.heightInPixels);
 
@@ -154,18 +180,20 @@ export class PlayScene extends Phaser.Scene{
         this.cameras.main.startFollow(this.player);
 
 
-        //If it gets the character, character dies
+
+       /* //If it gets the character, character decreases healthpoint until he is dead
         let collider = this.physics.add.overlap(this.wolf, this.player, (overlaped) =>{
             //stop when they overplay, kill the player(test)
             overlaped.body.stop();
-            this.player.kill();
+            this.player.healthPoints--;
+            if(this.player.healthPoints<=0){
+            this.player.kill();}
             this.physics.world.removeCollider(collider);
-        }, null, this);
-
+        }, null, this);*/
     }
 
     update(time,delta) {
-        
+        console.log( this.player_scale)
         //key control
         //movement note: we should only be able to move our character when it is alive
 
@@ -185,10 +213,7 @@ export class PlayScene extends Phaser.Scene{
                 this.player.setVelocityX(64);
  
             }
-            if (Phaser.Input.Keyboard.JustDown(this.spacebar))
-            {
-                this.player.attack();
-            }
+          
             if(this.keyboard.W.isUp && this.keyboard.S.isUp){
                 this.player.setVelocityY(0);
 
@@ -208,14 +233,44 @@ export class PlayScene extends Phaser.Scene{
             }
 
 
-            if(this.player.body.velocity.x != 0 || this.player.body.velocity.y != 0){
+            if(this.player.body.velocity.x !== 0 || this.player.body.velocity.y !== 0){
                 this.player.nonZeroVelocity = {x:this.player.body.velocity.x,y:this.player.body.velocity.y}; 
                 //velocity unless the actual velocity is zero then it stores previous nonzero velocity
                 //Need this value to keep track of the current direction when player is standing still. Prob will chage this later to direction
             }
-        }
+            //Generate player ability and skills
+            if (Phaser.Input.Keyboard.JustDown(this.spacebar))
+            {
+                this.player.attack();
+            }
+            //speed up the movement 
+            if(this.keyboard.SHIFT.isDown&this.keyboard.W.isDown)
+            {
+                this.player.setVelocityY(-192);
+            }
+            if(this.keyboard.SHIFT.isDown&this.keyboard.A.isDown)
+            {
+                this.player.setVelocityX(-192);
+            }
+            if(this.keyboard.SHIFT.isDown&this.keyboard.S.isDown)
+            {
+                this.player.setVelocityY(192);
+            }
+            if(this.keyboard.SHIFT.isDown&this.keyboard.D.isDown)
+            {
+                this.player.setVelocityX(192);
+            }
+            //still need to work on how to combine the two using only one key
+            if (Phaser.Input.Keyboard.JustDown(this.Rbar))
+            {
+                this.player.setScale(2);
+            }
+            if (Phaser.Input.Keyboard.JustDown(this.Tbar))
+            {
+                this.player.setScale(1);
+            }
+    
 
 
-    }
+    }}}
 
-}
