57266ccd4c8408787a03f613d4ea97cf43a47b91
mnmr1996 <mnmr1996@gmail.com>
2019-04-17 16:45:33 -0400

    got React to communicate and start the right scene outside of phaser

--- ReactProject/src/App.js
+++ ReactProject/src/App.js
@@ -12,7 +12,8 @@ class App extends Component {
     super(props);
     this.state = {
       showGame:false, 
-      showGame2:false,
+      showsingle:false,
+      showmulti:false, 
       hideButton:true,
       showbuttons: false,
       infobutton: true 
@@ -29,18 +30,28 @@ class App extends Component {
     this.setState({ showGame: !this.state.showGame });
   }
 
-  showGameHandler = () => {
+  showSinglePlayerHandler = () => {
     this.setState({ 
-      showGame2: !this.state.showGame2,
-      showbuttons: !this.state.showbuttons});
+      showsingle: !this.state.showsingle,
+      showbuttons: !this.state.showbuttons,
+      infobutton: !this.state.infobutton});
   }
   
+  showMultiPlayerHandler = () => {
+    this.setState({ 
+      showmulti: !this.state.showmulti,
+      showbuttons: !this.state.showbuttons,
+      infobutton: !this.state.infobutton});
+  }
+
   startingpage = () => {
     this.setState({
       showGame:true,
-      showGame2:false, 
+      showsingle:false,
+      showmulti:false,  
       hideButton:true,
-      showbuttons: true,  
+      showbuttons: true,
+      infobutton: true   
     })
   }
   
@@ -51,7 +62,7 @@ class App extends Component {
 
     return (
       <div className="App">
-      {!this.state.showGame2  &&
+      {!this.state.showsingle && !this.state.showmulti  &&
       <nav className="topbar">
         <ul>
           <li><button id="leader">Leaderboards</button></li>
@@ -64,21 +75,30 @@ class App extends Component {
       {this.state.showbuttons && (
       <div className="mostButtons">
       <ul>
-      <li><button onClick ={this.showGameHandler}>Single Player</button></li>
-      <li><button onClick ={this.showGameHandler}>Multiplayer</button></li>
+      <li><button onClick ={this.showSinglePlayerHandler}>Single Player</button></li>
+      <li><button onClick ={this.showMultiPlayerHandler}>Multiplayer</button></li>
       <li><button>store</button></li>
       <li><button>tutorial</button></li>
       </ul>
       </div>
       )}
 
-      {this.state.showGame2 && (
+      {this.state.showsingle && (
         <div>
-        <Game />
+        <Game name={"singleplayer"}/>
         <button onClick={this.startingpage}>back</button>
         </div>
       )}
+
+      {this.state.showmulti && (
+        <div>
+       <Game name={"multiplayer"}/>
+        <button onClick={this.startingpage}>back</button>
+        </div>
+      )}
+      {this.state.infobutton &&
       <button className="infobutton">info</button>
+      }
       </div>
     );
   }
