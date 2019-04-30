13c399d319d58981f68cf0c855c831af9a166ae4
mnmr1996 <mnmr1996@gmail.com>
2019-04-30 11:52:27 -0400

    testing the homepage startup buttons

--- ReactProject/src/Tests/App.test.js
+++ ReactProject/src/Tests/App.test.js
@@ -9,3 +9,13 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test('starting app test', () => {
+  const value = new App();
+  expect(value.state.showGame).toBe(false);
+  expect(value.state.showsingle).toBe(false);
+  expect(value.state.showmulti).toBe(false);
+  expect(value.state.hideButton).toBe(true);
+  expect(value.state.showbuttons).toBe(false);
+  expect(value.state.infobutton).toBe(true);
+})
