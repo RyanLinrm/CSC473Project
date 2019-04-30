36582f877b0e754ab0966aaebb1e9902dd0d8cbc
RyanLinrm <runminlin@gmail.com>
2019-04-30 17:51:54 -0400

    move file to root

---
+++ src/PhaserGame/CST.js
@@ -0,0 +1,11 @@
+export const CST ={
+    SCENES : {
+        LOAD: "LOAD",
+        LOAD2: "LOAD2",
+        MENU: "MENU",
+        PLAY: "PLAY",
+        WAIT: "WAIT",
+        PLAYMULTIPLAYER: "PLAYMULTIPLAYER",
+        CHAR: "CHAR"    
+    }
+}
