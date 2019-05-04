981e993643bcd0481c311af52977ef2ce2d52b5d
nmaswood98 <nmaswood98@gmail.com>
2019-05-04 17:42:49 -0400

    JSDOC comments for createSpecialWeapon, specialAttack, removeSpecialWeapon, createWeapon, attack, removeWeapon, kill and takeDamage

--- src/PhaserGame/gameObjects/Player.js
+++ src/PhaserGame/gameObjects/Player.js
@@ -2,7 +2,7 @@ import { Bullet } from "./Projectiles";
 import Phaser from 'phaser';
 
 /**
- * Player Class. 
+ * Player Class. The actual player sprite that gets added the the scene which is controlled by the user or bot or player data from the database
  */
 export class Player extends Phaser.Physics.Arcade.Sprite{
     /**
@@ -41,11 +41,28 @@ export class Player extends Phaser.Physics.Arcade.Sprite{
         this.setSize(34, 36);
     }
 
+    /**
+     * Intializes the special weapon of the player that is called during player construction
+     * Creates two functions specialAttack and removeSpecialWeapon
+     * Creates a group of bullets that is used when the player is attacked. 
+     * 
+     * 
+     * @param {Phaser.Scene} scene - The scene that the player is inside that is used to create the bullet group
+     */
     createSpecialWeapon(scene){ //Need to limit range of attack
         let bullets = scene.physics.add.group({classType: Bullet, runChildUpdate: true});
         
         let canAttack = false;
 
+
+        /**
+         * (Function is created by the createSpecialWeapon function) 
+         * 
+         * 
+         * specialAttack is a function which starts the attack and shoots the bullets corresponding to the behavior of the shooting
+         * If the mana is >10 the player can atttack and it decreases by 10 per attack. If you have less then 10 mana you can't
+         * The behavior of the attack is shooting bullets diagonally
+         */
         this.specialAttack = () => {
             
             if (this.mana >= 10) {
@@ -71,6 +88,14 @@ export class Player extends Phaser.Physics.Arcade.Sprite{
 
         }
 
+        /**
+         * (Function is created by the createSpecialWeapon function) 
+         * 
+         * 
+         * removeSpecialWeapon removes the special weapon from being shot and disables it from the plaeyer
+         * destroys the bullets group which is used for the special attack shooting.
+         * It also sets the specialAttack funciton to null
+         */
         this.removeSpecialWeapon = ()=>{ //destroys the weapon used
             bullets.destroy();
             this.specialAttack = null;
@@ -78,10 +103,22 @@ export class Player extends Phaser.Physics.Arcade.Sprite{
         
     }
 
-    
+    /**
+     * Intializes the weapon of the player so that the player can shoot
+     * creates the bullets which is added to the scene
+     * creates the attack function and removeWeapon funciton for the player
+     * 
+     * @param {Phaser.Scene} scene - The scene that the player is inside that is used to create the bullet group inside the createWeapon function
+     */
     createWeapon(scene){
         let bullets = scene.physics.add.group({classType: Bullet, runChildUpdate: true});
 
+        /**
+         * (Function is created by the createWeapon function)
+         * 
+         * 
+         * calling attack will shoot a bullet in the direction that the player is facing. 
+         */
         this.attack = ()=>{
             let bullet = bullets.get();
           //  scene.children.add(bullet);
@@ -89,6 +126,9 @@ export class Player extends Phaser.Physics.Arcade.Sprite{
             bullet.shoot(this.uid,this,this.nonZeroVelocity);
         };
 
+        /**
+         * calling removeWeapon destroys the weapon used by the player sets attack back to null
+         */
         this.removeWeapon = ()=>{ //destroys the weapon used
             bullets.destroy();
             this.attack = null;
@@ -96,12 +136,20 @@ export class Player extends Phaser.Physics.Arcade.Sprite{
 
     }
 
+    /**
+     * Removes a player so we can handle other things related to the death such as removing the wepopn    
+     */
     kill(){
-        //Remove a player so we can handle other things related to the death such as removing the wepopn    
         console.log(this);
         this.destroy();
     }
 
+    /**
+     * damages the player by the the given number supplied to the funciton
+     * If the ending healthpoints is less than 0 it calls the kill function
+     * 
+     * @param {number} damage - the amount of damage the player should take
+     */
     takeDamage(damage){
         if(this.canbeAttacked===true){
         this.healthPoints = this.healthPoints - damage;
