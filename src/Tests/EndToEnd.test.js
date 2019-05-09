143794055e22b249c5ad3ed1dbbd9422711820e6
Hongjie Huang <hongjie824@gmail.com>
2019-05-09 17:13:35 -0400

    Got the e2e test to work, will add more

--- src/Tests/EndToEnd.test.js
+++ src/Tests/EndToEnd.test.js
@@ -1,21 +1,28 @@
 const puppeteer = require('puppeteer');
-try {
-    (async () => {
+import { Authenticator ,withAuthenticator} from 'aws-amplify-react';
+test("Should show the correct game page",async()=>{
+    jest.setTimeout(1000000);
+    const browser =await puppeteer.launch({
+        headless:false,
+        slowMo:80,
+        args:['--window-size=1920,1080']
+    });
+    const page =await browser.newPage();
+    await page.goto('https://dev.d39k5usv8gv0fp.amplifyapp.com/');
+    await page.close();
+    
+});
+
+test("Should open the sign in correctly and enter username/password",async()=>{
+jest.setTimeout(1000000);
+const browser =await puppeteer.launch({
+    headless:false,
+    slowMo:80,
+    args:['--window-size=1920,1080']
+});
+const page =await browser.newPage();
+await page.goto('https://dev.d39k5usv8gv0fp.amplifyapp.com/');
+await page.click('button.col-md-2.btn.btn-success');
 
 
-        const browser = await puppeteer.launch({
-            headless: false,
-            slowMo: 500,
-            devtools: true,
-        });
-        const page = await browser.newPage();
-
-
-        await page.goto('localhost:3000');
-        await browser.close();
-
-    })();
-}
-catch (e) {
-
-}
+});
