bc7caadbe134576986c11d6cdb54d1b780acfdc2
Hongjie Huang <hongjie824@gmail.com>
2019-05-09 18:33:22 -0400

    Add more e2e, which need to update dev to work

--- src/Tests/EndToEnd.test.js
+++ src/Tests/EndToEnd.test.js
@@ -10,7 +10,7 @@ test("Should show the correct game page",async()=>{
     });
     const page =await browser.newPage();
     await page.goto('https://dev.d39k5usv8gv0fp.amplifyapp.com/');
-    await page.close();
+    await browser.close();            
     
 });
 
@@ -27,8 +27,7 @@ const browser =await puppeteer.launch({
     await page.goto('https://dev.d39k5usv8gv0fp.amplifyapp.com/');
     //the name after click is the name for button found from DOM
     await page.click('button.col-md-2.btn.btn-success');
-    await page.close();
-
+    await browser.close();
 });
 
 test("Should open the multiplayer scene correctly",async()=>{
@@ -41,8 +40,7 @@ test("Should open the multiplayer scene correctly",async()=>{
     const page =await browser.newPage();
     await page.goto('https://dev.d39k5usv8gv0fp.amplifyapp.com/');
     await page.click('button.col-md-2.btn.btn-primary');
-    await page.close();
-    
+    await browser.close();    
     });
 
 test("Should open the Tutorial scene correctly",async()=>{
@@ -55,8 +53,7 @@ test("Should open the Tutorial scene correctly",async()=>{
     const page =await browser.newPage();
     await page.goto('https://dev.d39k5usv8gv0fp.amplifyapp.com/');
     await page.click('button.col-md-2.btn.btn-info');
-    await page.close();
-        
+    await browser.close();        
     });
 
 test("Should open the Sign in page correctly",async()=>{
@@ -69,8 +66,38 @@ test("Should open the Sign in page correctly",async()=>{
     const page =await browser.newPage();
     await page.goto('https://dev.d39k5usv8gv0fp.amplifyapp.com/');
     await page.click('a.nav-link');
-    await page.close();
-            
+    await browser.close();            
 });
 
+/*
+test("Should open the leaderboard page correctly",async()=>{
+    jest.setTimeout(1000000);
+    const browser =await puppeteer.launch({
+        headless:false,
+        slowMo:80,
+        args:['--window-size=1920,1080']
+    });
+    const page =await browser.newPage();
+    await page.goto('https://dev.d39k5usv8gv0fp.amplifyapp.com/');
+    await page.click('a.leaderboard.nav-link');
+    await browser.close();            
+});*/
 
+/* This one is not working because the dev webiste's leaderboard and sign in has the same 
+button name, which can be changed in the app.js file, need to test when the dev is updated
+test("Should open the Sign in page correctly and enter user name and password",async()=>{
+    jest.setTimeout(1000000);
+    const browser =await puppeteer.launch({
+        headless:false,
+        slowMo:80,
+        args:['--window-size=1920,1080']
+    });
+    const page =await browser.newPage();
+    await page.goto('https://dev.d39k5usv8gv0fp.amplifyapp.com/');
+    await page.click('a.signin.nav-link');
+    await page.waitForSelector('div.Form__formContainer___cu04J');
+    await page.click('input.Input__input___2Sh1s');
+    await page.type('input.Input__input___2Sh1s','username');
+            
+});
+*/
