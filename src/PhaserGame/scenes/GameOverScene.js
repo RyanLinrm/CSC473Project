306b36b716459369bd3380946cf51390e223392c
mnmr1996 <mnmr1996@gmail.com>
2019-05-11 14:43:55 -0400

    you can now restart the game after you lose

--- src/PhaserGame/scenes/GameOverScene.js
+++ src/PhaserGame/scenes/GameOverScene.js
@@ -23,6 +23,32 @@ export class GameOverScene extends Phaser.Scene{
         LosingText.setOrigin(0.5,0.5); 
         let dataText = this.add.text(width/2, height/2+100, `Player ID : ${this.playerID}`);
         this.add.text(width/2, height/2+150, `Your character : ${this.charType}`);
-        this.add.text(width/2, height/2+200, `Your final score : ${this.score}`);
+        this.add.text(width/2-40, height/2+200, `Your final score : ${this.score}`);
+
+       
+        //adding in a restart button
+        let buttonCursor = this.add.image(100, 100, "cursor");
+        buttonCursor.setScale(0.03);
+        buttonCursor.setVisible(false);
+
+
+        let anothergame = this.add.text(width/2, height/2+250, 'Play Again?', { fontFamily: 'Arial', fontSize: 60, color: '#ffffff' });
+        anothergame.setInteractive();
+
+        anothergame.on("pointerup", () => {
+            this.scene.start(CST.SCENES.MULTIPLAYERCHARSELECT);
+          });
+
+        anothergame.on("pointerover", ()=>{
+            buttonCursor.setVisible(true);
+            buttonCursor.x = anothergame.x - 60;
+            buttonCursor.y = anothergame.y + 30;
+        });
+
+        anothergame.on("pointerout", ()=>{
+            buttonCursor.setVisible(false);
+        });
+
+        
     }
 }
